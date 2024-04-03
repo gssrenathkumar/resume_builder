bba663dd3071b3270133993c806e3fb0ff3aaafb
G S SRENATH KUMAR <70089532+gssrenathkumar@users.noreply.github.com>
2024-04-03 17:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import re
 import all_functions as func
 import shutil
 import time
-# Configure Google generative AI
+from zipfile import ZipFile
 
 
 # Set page configuration
@@ -19,7 +19,6 @@ st.set_page_config(page_title="Resume Data Extractor")
 st.header("Resume Data")
 
 api_key = st.text_input('Enter your API key', value='AIzaSyAbGYl1RWYku3ntot7fWhRXjwZlwNOJzvc')
-
 genai.configure(api_key=api_key)
 
 def image_processing_genai(uploaded_files):
@@ -56,86 +55,47 @@ def process_resume(uploaded_files, filename):
         else:
             st.error("Unsupported file format. Please upload a PDF, DOCX, JPEG, or PNG file.")
             return
-
-
-        st.text_area("Text extracted from file:", resume_text)
-
-        work_experience = func.get_work_experience_response(resume_text)
-        st.subheader("Work Experience or Internship Experience:")
-        st.write(work_experience)
-
         # Get name response
         name = func.get_name_response(resume_text)
-        st.subheader("Name extracted from resume:")
-        st.write(name)
+        work_experience = func.get_work_experience_response(resume_text)
 
         # Get summary response
-        summary = func.get_summary_response(resume_text)
-        st.subheader("Experience Summary:")
-        st.write(summary)
-
-        
-
+        summary = func.get_summary_response(resume_text)     
         # Get certifications response
         certifications = func.get_certifications_response(resume_text)
-        st.subheader("Certifications:")
-        st.write(certifications)
-
         # Get degree details response
         degree_details = func.get_degree_details_response(resume_text)
-        st.subheader("Degree Details:")
-        st.write(degree_details)
-
         # Get education details response
         education_details = func.get_education_details_response(resume_text)
-        st.subheader("Education Details:")
-        st.write(education_details)
-
         # Get education years in descending order response
         education_years_descending = func.get_education_years_response(resume_text)
-        st.subheader("Education Years (Descending Order):")
-        st.write(education_years_descending)
-
         # Get technical skills response
         technical_skills = func.get_technical_skills_response2(resume_text)
-        st.subheader("Technical Skills:")
-        st.write(technical_skills)
-
-       # Fill in the Word document template for all sections
+       
+        # Fill in the Word document template for all sections
         template_path = 'Templates/agilisium_template.docx'
         output_path = f'agilisium_resume_internal_template/{filename}_resume.docx'
         func.fill_invitation(template_path, output_path, name, summary, certifications)
-        st.success("Details have been filled in the document!")
 
         # Fill in the degree details inside the table
         func.fill_table_degree_details(output_path, output_path, degree_details)
-        st.success("Degree details have been filled in the document!")
-
         # Fill in the institute details inside the table
         func.fill_table_institute_details(output_path, output_path, education_details)
-        st.success("Institute details have been filled in the document!")
-
         # Fill in the education years inside the table
         func.fill_table_education_years(output_path, output_path, education_years_descending)
-        st.success("Education years have been filled in the document!")
-
         # Fill in the skill set inside the existing table
         func.fill_table_skill_set(output_path, output_path, technical_skills)
-        st.success("Skill set has been filled in the document!")
-
         func.replace_organization_count(output_path, output_path,work_experience)
         
         updated_doc = func.replace_hyphens_with_bullet_points(output_path)
         updated_doc.save(output_path)
         func.remove_characters_from_docx2(output_path)
-        
         updated_doc1 = func.replace_symbol_with_dash(output_path)
         updated_doc1.save(output_path)
-        
         time.sleep(5)
         func.func.process_docx11(output_path)
         func.delete_rows_with_any_empty_cells(output_path)
-        
+        st.success(f"{filename}-document processed successfully.")
         
         
 
@@ -158,62 +118,22 @@ def process_resume_2(uploaded_files, filename):
             st.error("Unsupported file format. Please upload a PDF, DOCX, JPEG, or PNG file.")
             return
 
-        st.text_area("Text extracted from file:", resume_text)
         
-        # Get summary response
         summary2 = func.get_summary_response(resume_text)
-        st.subheader("Experience Summary:")
-        st.write(summary2)
-
         project_experience = func.relevant_project_experience(resume_text)
-        st.subheader("Project Revelant Experience")
-        st.write(project_experience)
-        
         skills1 = func.get_technical_skills_response2(resume_text)
-        st.subheader("Technical Skills:")
-        st.write(skills1)
-        
-        
-        
-        # Get certifications response
         certifications1 = func.get_certifications_response(resume_text)
-        st.subheader("Certifications:")
-        st.write(certifications1)
-        
+
         template_path = 'Templates/Client sample format.docx'
         output_path = f'agilisium_resume_client_format/{filename}_resume.docx'
         summar = "DASdSDAas"
         func.fill_invitation2(template_path, output_path,summary2,skills1,project_experience,certifications1,summar)
-        st.success("Details have been filled in the document!")
-        
-        # Get degree details response
         degree_details1 = func.get_degree_details_response(resume_text)
-        st.subheader("Degree Details:")
-        st.write(degree_details1)
-
-        # Get education details response
         education_details1 = func.get_education_details_response(resume_text)
-        st.subheader("Education Details:")
-        st.write(education_details1)
-
-        # Get education years in descending order response
         education_years_descending1 = func.get_education_years_response(resume_text)
-        st.subheader("Education Years (Descending Order):")
-        st.write(education_years_descending1)
-
-
-        # Fill in the degree details inside the table
         func.fill_table_degree_details(output_path, output_path, degree_details1)
-        st.success("Degree details have been filled in the document!")
-
-        # Fill in the institute details inside the table
         func.fill_table_institute_details(output_path, output_path, education_details1)
-        st.success("Institute details have been filled in the document!")
-
-        # Fill in the education years inside the table
         func.fill_table_education_years(output_path, output_path, education_years_descending1)
-        st.success("Education years have been filled in the document!")
-
         updated_doc = func.replace_hyphens_with_bullet_points(output_path)
         updated_doc.save(output_path)
 
@@ -221,7 +141,7 @@ def process_resume_2(uploaded_files, filename):
         func.process_docx11(output_path)
         func.delete_rows_with_any_empty_cells(output_path)
         func.remove_characters_from_docx2(output_path)
-        
+        st.success(f"{filename}-document processed successfully.")
         
     except FileNotFoundError as e:
         st.error(str(e))
@@ -243,80 +163,37 @@ def process_resume_3(uploaded_files,filename):
             st.error("Unsupported file format. Please upload a PDF, DOCX, JPEG, or PNG file.")
             return
         
-        st.text_area("Text extracted from file:", resume_text)
-        
         # Get name response
         name1 = func.get_name_response(resume_text)
-        st.subheader("Name extracted from resume:")
-        st.write(name1)
-        
         # Get summary response
         summary3 = func.get_summary_response(resume_text)
-        st.subheader("Experience Summary:")
-        st.write(summary3)
-        
         # Get technical skills response
         technical_skills3 = func.get_technical_skills_response2(resume_text)
-        st.subheader("Technical Skills:")
-        st.write(technical_skills3)
-                
+        
         template_path = 'Templates/Client sample format-2.docx'
         output_path = f'agilisium_resume_client_format_2/{filename}_resume.docx'
-
         func.fill_invitation3(template_path, output_path, name1, summary3)
-        st.success("Details have been filled in the document!")
-
-        # Fill in the skill set inside the existing table
         func.fill_table_skill_set(output_path, output_path, technical_skills3)
-        st.success("Skill set has been filled in the document!")
         
-        # Get degree details response
         degree_details1 = func.get_degree_details_response(resume_text)
-        st.subheader("Degree Details:")
-        st.write(degree_details1)
-
-        # Get education details response
         education_details1 = func.get_education_details_response(resume_text)
-        st.subheader("Education Details:")
-        st.write(education_details1)
-
-        # Get education years in descending order response
         education_years_descending1 = func.get_education_years_response(resume_text)
-        st.subheader("Education Years (Descending Order):")
-        st.write(education_years_descending1)
-        
-        # Fill in the degree details inside the table
         func.fill_table_degree_details(output_path, output_path, degree_details1)
-        st.success("Degree details have been filled in the document!")
-
-        # Fill in the institute details inside the table
         func.fill_table_institute_details(output_path, output_path, education_details1)
-        st.success("Institute details have been filled in the document!")
-
-        # Fill in the education years inside the table
         func.fill_table_education_years(output_path, output_path, education_years_descending1)
-        st.success("Education years have been filled in the document!")
         
         work_experience2 = func.get_work_experience_response2(resume_text)
-        st.subheader("Work Experience or Internship Experience:")
-        st.write(work_experience2)
         func.replace_organization_count2(output_path, output_path,work_experience2)
         
         updated_doc = func.replace_hyphens_with_bullet_points(output_path)
         updated_doc.save(output_path)
         func.remove_characters_from_docx2(output_path)
-
         updated_doc1 = func.replace_symbol_with_dash(output_path)
         updated_doc1.save(output_path)
-        
         time.sleep(5)
         func.func.process_docx11(output_path)
         func.delete_rows_with_any_empty_cells(output_path)
 
-
-
-        
-        
         # Task from pdf to image
         filename = func.save_document(uploaded_files)
         filename = func.convert_to_pdf_if_docx(filename)
@@ -327,40 +204,10 @@ def process_resume_3(uploaded_files,filename):
         func.replace_placeholder_with_image(output_path,image_path)
         func.remove_pdf_and_docx_files_in_script_directory()
         
+        st.success(f"{filename}-document processed successfully.")
+        
     except FileNotFoundError as e:
-        st.error(str(e))
-        
-        
-    finally:
-        from PIL import Image
-        def create_temp_document(path):
-            img = Image.new('RGB', (800, 600), color = 'white')
-            # Save the image to the path specified
-            img.save(path)
-            
-        create_temp_document(out_path1)
-        create_temp_document(image_path)
-        try:
-            if temp_pdf_path:
-                os.remove(filename)
-        except FileNotFoundError:
-            pass  # File doesn't exist, ignore the error
-        
-        try:
-            if out_path1:
-                os.remove(out_path1)
-        except FileNotFoundError:
-            pass  # File doesn't exist, ignore the error
-        
-        try:
-            if image_path:
-                os.remove(image_path)
-        except FileNotFoundError:
-            pass  # File doesn't exist, ignore the error
-
-    
-
-        
+        st.error(str(e))      
         
 # Function to process and save DOCX file
 def process_and_save(uploaded_files, process_func):
@@ -378,23 +225,13 @@ def internal_template_button(uploaded_files):
         process_and_save(uploaded_files, process_resume)
 
 def client_template_button(uploaded_files):
-    st.write("Client Template button clicked.")
     if uploaded_files is not None:
         process_and_save(uploaded_files, process_resume_2)
 
 def client_template_with_photo_button(uploaded_files):
-    # Functionality for client template with photo button
-    st.write("Client Template with Photo button clicked.")
     if uploaded_files is not None:
         process_and_save(uploaded_files, process_resume_3)
     
-
-import streamlit as st
-from zipfile import ZipFile
-from io import BytesIO
-import os
-import shutil
-
 
 
 # File upload
